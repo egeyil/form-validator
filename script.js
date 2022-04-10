0f45efbef8869e2a52de4cc03e5c2fc226dcb322
Ege Yildirim <egeyildirimdevelopment@gmail.com>
2022-04-10 13:39:30 +0300

    added html, css and basic js

---
+++ script.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError (input, message) {
+    const formControl = input.parentElement;
+    formControl.classList.add('error');
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// Show success outline 
+function showSuccess (input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Check email is valid 
+function isValidEmail (email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+// ================ Event Listeners =================
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault(); //otomatik submitlemesini engelliyoruz
+
+    if(username.value === '') { // If you don't use .value it will return a html element
+        showError(username, 'Username is required');
+    }   else {
+        showSuccess(username);
+    }
+});
